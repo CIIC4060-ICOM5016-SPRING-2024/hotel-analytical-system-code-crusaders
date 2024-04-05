89a13a3dae9eb2f916e5d3ed0ff1bf13391a4147
saul Figueroa <saul.figueroa1@upr.edu>
2024-04-05 17:29:43 -0400

    moved Client and Reserve to root folders.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from controller.hotels import Hotels
 from controller.chains import Chains
 from controller.employee import Employee
+from controller.clients import clients
+from controller.reserve import reserve
 
 app = Flask(__name__)
 CORS(app)
@@ -78,6 +80,53 @@ def handleEmployeesbyID(eid):
         return handler.updateEmployee(request.json)
     elif request.method == 'DELETE':
         return handler.deleteEmployee(eid)
+
+
+##################################################
+#                    ClIENT
+##################################################
+@app.route('/codecrusaders/client', methods = ['GET', 'POST'])
+def handle_clients():
+    handler = clients()
+    if request.method == 'GET':
+        return clients().getAllClients()
+    elif request.method == 'POST':
+        return handler.addNewClient(request.json)
+
+
+@app.route('/codecrusaders/client/<int:clid>', methods = ['GET', 'PUT', 'DELETE'])
+def handle_clientById(clid):
+    if request.method == 'GET':
+        return clients().getClientByID(clid)
+    elif request.method == 'PUT':
+        return clients().updateClient(request.json)
+    elif request.method == 'DELETE':
+        return clients().deleteClient(clid)
+    else:
+        return jsonify("method Not Allowed"), 405
+
+
+##################################################
+#                 Reserve
+##################################################
+@app.route('/codecrusaders/reserve', methods = ['GET', 'POST'])
+def handle_reserve():
+    if request.method == 'GET':
+        return reserve().getAllReserve()
+    elif request.method == 'POST':
+        return reserve().addNewReserve(request.json)
+
+
+@app.route('/codecrusaders/reserve/<int:reid>', methods = ['GET', 'PUT', 'DELETE'])
+def handle_reserveById(reid):
+    if request.method == 'GET':
+        return reserve().getReserveById(reid)
+    elif request.method == 'PUT':
+        return reserve().updateReserve(request.json)
+    elif request.method == 'DELETE':
+        return reserve().deleteReserve(reid)
+    else:
+        return jsonify("method Not Allowed"), 405
 
 
 if __name__ == '__main__':
