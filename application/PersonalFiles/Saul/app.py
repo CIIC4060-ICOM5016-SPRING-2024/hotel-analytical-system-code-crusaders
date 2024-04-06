dc948a2a1312da130b88b934b9a20334e6571b78
saul Figueroa <saul.figueroa1@upr.edu>
2024-04-06 17:46:43 -0400

    added routes and handler for local and global statistic

--- application/PersonalFiles/Saul/app.py
+++ application/PersonalFiles/Saul/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from controller.hotels import Hotels
+from controller.chains import Chains
+from controller.employee import Employee
 from controller.clients import clients
 from controller.reserve import reserve
+from controller.local_stat import local_stat
+from controller.global_stat import global_stat
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +16,74 @@ CORS(app)
 def hello_world():
     return "Hello World!"
 
+#############################################
+#                HOTEL
+#############################################
+
+
+@app.route('/codecrusaders/hotel', methods=['GET', 'POST'])
+def handleHotels():
+    handler = Hotels()
+    if request.method == 'POST':
+        return handler.createHotel(request.json)
+    else:
+        return handler.getAllHotels()
+
+@app.route('/codecrusaders/hotel/<int:hid>', methods=['GET','PUT','DELETE'])
+def handleHotelsbyID(hid):
+    handler = Hotels()
+    if request.method == 'GET':
+        return handler.getHotelbyID(hid)
+    elif request.method == 'PUT':
+        return handler.updateHotel(request.json)
+    elif request.method == 'DELETE':
+        return handler.deleteHotel(hid)
+
+#############################################
+#                CHAINS
+#############################################
+
+
+@app.route('/codecrusaders/chains', methods=['GET', 'POST'])
+def handleChains():
+    handler = Chains()
+    if request.method == 'POST':
+        return handler.createChain(request.json)
+    else:
+        return handler.getAllChains()
+
+@app.route('/codecrusaders/chains/<int:chid>', methods=['GET','PUT','DELETE'])
+def handleChainsbyID(chid):
+    handler = Chains()
+    if request.method == 'GET':
+        return handler.getChainbyID(chid)
+    elif request.method == 'PUT':
+        return handler.updateChain(request.json)
+    elif request.method == 'DELETE':
+        return handler.deleteChain(chid)
+
+
+#############################################
+#                EMPLOYEE
+#############################################
+
+
+@app.route('/codecrusaders/employee', methods=['GET', 'POST'])
+def handleEmployees():
+    handler = Employee()
+    if request.method == 'POST':
+        return handler.createEmployee(request.json)
+    else:
+        return handler.getAllEmployees()
+@app.route('/codecrusaders/employee/<int:eid>', methods=['GET','PUT','DELETE'])
+def handleEmployeesbyID(eid):
+    handler = Employee()
+    if request.method == 'GET':
+        return handler.getEmployeebyID(eid)
+    elif request.method == 'PUT':
+        return handler.updateEmployee(request.json)
+    elif request.method == 'DELETE':
+        return handler.deleteEmployee(eid)
 
 ##################################################
 #                    ClIENT
@@ -22,6 +95,8 @@ def handle_clients():
         return clients().getAllClients()
     elif request.method == 'POST':
         return handler.addNewClient(request.json)
+    else:
+        return jsonify("method Not Allowed"), 405
 
 
 @app.route('/codecrusaders/client/<int:clid>', methods = ['GET', 'PUT', 'DELETE'])
@@ -45,6 +120,8 @@ def handle_reserve():
         return reserve().getAllReserve()
     elif request.method == 'POST':
         return reserve().addNewReserve(request.json)
+    else:
+        return jsonify("method Not Allowed"), 405
 
 
 @app.route('/codecrusaders/reserve/<int:reid>', methods = ['GET', 'PUT', 'DELETE'])
@@ -57,6 +134,32 @@ def handle_reserveById(reid):
         return reserve().deleteReserve(reid)
     else:
         return jsonify("method Not Allowed"), 405
+
+
+##################################################
+#                LOCAL STATISTICS
+##################################################
+@app.route('/codecrusaders/hotel/<int:hid>/highestpaid', methods = ['POST'])
+def handle_hotel_highest_paid(hid):
+    if request.method == 'POST':
+        return local_stat().getHighestPaid(hid, request.json)
+    else:
+        return jsonify("method Not Allowed"), 405
+
+
+##################################################
+#                GLOBAL STATISTICS
+##################################################
+@app.route('/codecrusaders/most/capacity', methods = ['POST'])
+def handle_most_capacity():
+    return global_stat().getMostCapacity()
+
+
+@app.route('/codecrusaders/most/profitmonth', methods = ['POST'])
+def handle_most_profitmonth():
+    return global_stat().getMostProfitMonth()
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
