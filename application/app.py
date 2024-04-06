bc1c0c13bb39385b86783bff417729afab7023b3
Janice-Figueroa <janice.figueroa7@upr.edu>
2024-04-05 22:42:24 -0400

    finished app.py

--- application/app.py
+++ application/app.py
@@ -18,9 +18,24 @@ def home_page():
 ===============================================
 """
 
-@app.route('/room')
-def room():
-    return RoomController().getAllRooms()
+@app.route('/codecrusaders/room', methods = ['GET','POST'])
+def handle_room():
+    handler = RoomController()
+    if request.method == 'GET':
+        return RoomController().getAllRooms()
+    elif request.method == 'POST':
+        return handler.createRoombyID(request.json)
+    
+@app.route('/codecrusaders/room/<int:rid', method = ['GET','PUT','DELETE'])
+def handleRoomsbyID(rid):
+    handler = RoomController()
+    if request.method == 'GET':
+        return handler.getRoombyid(rid)
+    elif request.method == 'PUT':
+        return handler.updateRoombyID(request.json)
+    elif request.method == 'DELETE':
+        return handler.deleteRoombyID(rid)
+    
 
 """
 ===============================================
@@ -28,7 +43,25 @@ def room():
 ===============================================
 """
 
-@app.route('/roomdescription')
-def roomdescription():
-    return RoomUnavailableController.getRoomUnavailableby()
+@app.route('/codecrusaders/room_unavailable', methods=['GET', 'POST'])
+def handleRoomUnavailable():
+    handler = RoomUnavailableController()
+    if request.method == 'POST':
+        return handler.createRoomUnavailablebyID(request.json)
+    else:
+        return handler.getAllRoomsUnavailable()
+    
+@app.route('/codecrusaders/room_unavailable/<int:ruid>', methods=['GET','PUT','DELETE'])
+def handleRoomUnavailablebyID(ruid):
+    handler = RoomUnavailableController()
+    if request.method == 'GET':
+        return handler.getRoomUnavailablebyID(ruid)
+    elif request.method == 'PUT':
+        return handler.updateRoomUnavailablebyID(ruid)
+    elif request.method == 'DELETE':
+        return handler.deleteRoomUnavailablebyID(ruid)
+    
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
