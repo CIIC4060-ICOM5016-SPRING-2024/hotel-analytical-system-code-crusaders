e750c0f54543bf56eecfa24806145972e6834cc7
saul Figueroa <saul.figueroa1@upr.edu>
2024-04-08 18:26:15 -0400

    added global stats: mostCapacity, MosrProfitMonth.

--- model/StatisticsDAO.py
+++ model/StatisticsDAO.py
@@ -228,6 +228,11 @@ class StatisticsDAO:
     def get_MostCapacity(self):
         result = Database().querySelectFrom(
             """
+            select hname, sum(capacity) as total_capacity 
+            from hotel natural inner join room natural inner join roomdescription 
+            group by hid, hname 
+            order by total_capacity desc 
+            limit 5;
             """,
             ()
         )
@@ -244,6 +249,18 @@ class StatisticsDAO:
     def get_MostProfitMonth(self):
         result = Database().querySelectFrom(
             """
+            with extract_month_and_year as
+                        (select chid as chainID, cname as name, extract(YEAR from startdate) as year, extract(MONTH from startdate) as month
+                        from chains natural inner join hotel natural inner join room natural inner join roomunavailable),
+
+                        total_reservations_by_month_year as
+                            (select *, count(month) as total_reservations
+                            from extract_month_and_year group by chainID,month,year,name order by chainID, total_reservations desc)
+
+                    select name, year, month, total_reservations 
+                    from (select *, row_number() over (partition by chainID order by total_reservations desc)
+                    as highest from total_reservations_by_month_year) result
+                    where highest <= 3;
             """,
             ()
         )
